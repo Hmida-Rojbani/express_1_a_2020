e7266259c6ef7c104ce7509fce17910f134c64f9
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-01-25 11:50:16 +0100

    express_p3

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT||3000;
 
+let students = [
+    {id:1, name : "std1"},
+    {id:2, name : "std2"},
+    {id:3, name : "std3"}
+];
+
+
+app.get('/api/students', (req,res)=>{
+    res.send(students);
+});
+app.get('/api/students/:id', (req,res)=>{
+    const student = students.find(s=>s.id === parseInt(req.params.id));
+    if(!student)
+        return res.status(404).send('Student id not found');
+    res.send(student);
+});
+
+
+
 var quotes = {
     'einstein': 'The only reason for time is so that everything doesn\'t happen at once.',
     'tesla' : 'The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane'
